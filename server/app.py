06ba690fc4a4ad267ebc24712ed2a58a27b6ef14
lazarmarkov <lazarmarkov88@gmail.com>
2015-08-05 01:20:56 +0300

    Importing sample data from /data folder

--- server/app.py
+++ server/app.py
@@ -2,13 +2,18 @@
 
 import os
 import numpy as np
+import pandas as pd
 from flask import Flask
-from server.api import api
+from server.api import api, data_path
+from server.product import Product, PopularProductsService
+from server.shop import Shop, ShopRepository
+
 
 def create_app(settings_overrides=None):
     app = Flask(__name__)
     configure_settings(app, settings_overrides)
     configure_blueprints(app)
+    initialize(app)
     return app
 
 
@@ -26,4 +31,15 @@ def configure_settings(app, settings_override):
 
 def configure_blueprints(app):
     app.register_blueprint(api)
-    
+
+
+def initialize(app):
+    get_data = lambda f: pd.read_csv(os.path.join(app.config['DATA_PATH'], f)).get_values()
+
+    tags = {r[0]: r[1] for r in get_data('tags.csv')}
+    shops = [Shop(r[0], (r[2], r[3]), r[1]) for r in get_data('shops.csv')]
+    taggings = [(tags[r[2]], r[1]) for r in get_data('taggings.csv')]
+    products = [Product(r[0], r[1], r[2], r[3], r[4]) for r in get_data('products.csv')]
+
+    app.shop_repo = ShopRepository(shops, taggings)
+    app.prod_service = PopularProductsService(products)
